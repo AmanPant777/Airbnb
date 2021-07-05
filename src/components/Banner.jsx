3b4451345870822752c9ff08900512102f85bf56
AmanPant777 <footballeramanpt65@gmail.com>
2021-07-05 13:28:01 +0530

    Add Banner DatePicker
    Add DatePicker and function on butoon

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Banner.css'
 import { Button } from '@material-ui/core'
 const Banner = () => {
+    const [showSearch,setShowSearch]=useState(false)
     return (
-       <div className='banner'>
-         <div className='banner_info'>
-             <h1>Get out and stretch your imagination</h1>
-             <h5>Plan a different kind of gateway to uncover the hidden gems near you</h5>
-             <Button variant='outlined'>Explore Nearby</Button>
-         </div>
-       </div>
+        <div className='banner'>
+            <div className="banner_search">
+                {showSearch&&<h1>Show Date Picker</h1>}
+                <Button onClick={()=>setShowSearch(!showSearch)} className='banner_searchbutton' variant='outlined'>Search Dates</Button>
+                </div>
+            <div className='banner_info'>
+                <h1>Get out and stretch your imagination</h1>
+                <h5>Plan a different kind of gateway to uncover the hidden gems near you</h5>
+                <Button variant='outlined'>Explore Nearby</Button>
+            </div>
+        </div>
     )
-
 }
-
 export default Banner
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import Banner from './Banner'
 const Home = () => {
     return (
         <div className='home'>
-            <h1>Home Component</h1>
+            
             <Banner/>
         </div>
     )
