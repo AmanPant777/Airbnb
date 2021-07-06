2683ae00b85ecb61d96fd087c316994e00b8f972
AmanPant777 <footballeramanpt65@gmail.com>
2021-07-06 10:28:14 +0530

    Implementing Cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Home.css";
 import Banner from "./Banner";
+import {Card} from '@material-ui/core'
 const Home = () => {
   return (
     <div className="home">
       <Banner />
+      <div className="home_section">
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
+      <div className="home_section">
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
     </div>
   );
 };
